071649e874b4d8e35755ff17bcd86004ef2782da
wetpeach <wetpeach@wetpeachs-MBP.home>
2022-04-22 21:45:14 +0200

    checkpoint: filter vs async/await

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,55 +1,46 @@
-import React, { useState, useEffect } from "react";
-// import { update } from "../BooksAPI";
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // Components
-import Bookshelf from './Bookshelf'
+import Bookshelf from "./Bookshelf";
 
-function ListBooks({shelfHandler, listBooks}) {
-  const currentlyReading = listBooks.filter( book => (book.shelf === "currentlyReading"))
-  const wantToRead = listBooks.filter( book => (book.shelf === "wantToRead"))
-  const read = listBooks.filter( book => (book.shelf === "read"))
+function ListBooks({ shelfHandler, listBooks }) {
+  const currentlyReading = listBooks.filter(
+    (book) => book.shelf === "currentlyReading"
+  );
+  const wantToRead = listBooks.filter((book) => book.shelf === "wantToRead");
+  const read = listBooks.filter((book) => book.shelf === "read");
 
-  // const [state, setState] = useState([])
-
-
-  // const shelfHandler = async (item) => {
-  //   const value = item.target.value
-  //   const id = item.target.name
-  //   const response = await update({id:id}, value);
-  //   setState( response )
-  //   console.log("response", response)
-  // };
-
-
-  useEffect(() => {
-    // console.log("state",state)
-    // console.log("listBooks",listBooks)
-  },[listBooks])
-
-
-    return(
-
-      <div className="app">
-        <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf shelfHandler={shelfHandler} books={currentlyReading} name='Currently Reading' />
-                <Bookshelf shelfHandler={shelfHandler} books={wantToRead} name='Want to Read' />
-                <Bookshelf shelfHandler={shelfHandler} books={read} name='Read' />
-              </div>
-            </div>
-            <div className="open-search">
-                {/* "Circle Plus" Button */}
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
+  return (
+    <div className="app">
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Bookshelf
+              shelfHandler={shelfHandler}
+              books={currentlyReading}
+              name="Currently Reading"
+            />
+            <Bookshelf
+              shelfHandler={shelfHandler}
+              books={wantToRead}
+              name="Want to Read"
+            />
+            <Bookshelf shelfHandler={shelfHandler} books={read} name="Read" />
           </div>
-          </div>
-    )
+        </div>
+        <div className="open-search">
+          {/* "Circle Plus" Button */}
+          <Link to="/search">
+            <button>Add a book</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default ListBooks
+export default ListBooks;
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { search } from "../BooksAPI";
+import SearchTerms from "../BooksSearchTerms.json";
+import { Link } from "react-router-dom";
 
-function SearchBooks(){
-    return (
-        <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
+// components
+import BookItem from "./BookItem";
+
+function SearchBooks({shelfHandler}) {
+  const [term, setTerm] = useState("");
+  const [found, setFound] = useState([]);
+
+  const inputHandler = (query) => {
+    const value = query.target.value;
+    setTerm(value);
+    getSearch(query);
+  };
+
+  const getSearch = async (searchTerm) => {
+    const response = await getSearchTerms(searchTerm.target.value);
+    // console.log("getSearch", response);
+  };
+
+  const getSearchTerms = async (userquery) => {
+    let foundQuery = [];
+    const result = await SearchTerms.map(async (sTerm) => {
+      const response = await search(sTerm);
+      const filter = response.filter((item) => {
+        if (Object.keys(item).includes("authors")) {
+          item.authors.forEach((aItem) => {
+            if (aItem.includes(userquery)) foundQuery.push(item);
+            if (aItem.includes(userquery)) setFound(item);
+            if (aItem.includes(userquery)) console.log("filter loop:", item);
+          });
+        }
+      });
+      // console.log("result loop:", await filter)
+      console.log("result loop:", "foundQuery", foundQuery);
+      // return response
+      // return filter
+    });
+    // console.log("getSearchTerms",result)
+    // console.log("getSearchTerms",foundQuery)
+    // return result;
+    // console.log("getSearchTerms", result.then(value => (value)))
+    // console.log("getSearchTerms",result)
+    // console.log("getSearchTerms", result.map( item => item.then(value => (value))))
+    // console.log("getSearchTerms", result.map( item => {
+    //   item.then( value => console.log("value", value))
+    // }))
+    // return result.then(value => (value));
+  };
+
+  useEffect(() => {
+    // getSearch()
+  }, []);
+
+  // console.log("found", found)
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/">
+          <button className="close-search">Close</button>
+        </Link>
+        <div className="search-books-input-wrapper">
+          {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -14,14 +71,24 @@ function SearchBooks(){
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+          <input
+            type="text"
+            onChange={inputHandler}
+            value={term}
+            placeholder="Search by title or author"
+          />
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-    )
+          {found.length ? "true" : "false"}
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {
+            found.length && found.map( book => (<BookItem book={book} shelfHandler={shelfHandler} key={Math.random()}/>))
+          }
+        </ol>
+      </div>
+    </div>
+  );
 }
-export default SearchBooks
+export default SearchBooks;
