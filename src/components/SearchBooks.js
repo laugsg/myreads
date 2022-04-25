81f32204f64e9f9985a365a34e7c706fe580994a
laugsg <lautaro.grucci@joingsg.com>
2022-04-25 17:54:10 +0200

    update > fix to resubmit

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -5,25 +5,10 @@ import { search } from "../BooksAPI";
 import SearchInput from "./searchInput";
 import SearchClose from "./searchClose";
 import BookItem from "./BookItem";
-import Loader from "./Loader";
-
-// Search Terms
-import searchTerms from "../BooksSearchTerms.json";
 
 class SearchBooks extends Component {
   state = {
-    biblio: [],
-    found: []
-  };
-
-  obtainDatabase = () => {
-    searchTerms.forEach((term) => {
-      search(term).then((res) => {
-        this.setState((prevState) => ({
-          biblio: [...prevState.biblio, res],
-        }));
-      });
-    });
+    biblio: []
   };
 
   cleanFoundBooks = () => {
@@ -33,30 +18,16 @@ class SearchBooks extends Component {
   };
 
   searchTerm = (query) => {
-    this.cleanFoundBooks();
-    let booksArr = [];
-    this.state.biblio.map((booksArray) => {
-      booksArray.filter((book) => {
-        if (Object.keys(book).includes("authors")) {
-          book.authors.forEach((author) => {
-            if (author.includes(query)) {
-              booksArr.push(book)
-            }
-          });
-        }
-        if (book.title.includes(query)) {
-          booksArr.push(book)
-        }
+    if( query.length > 0){
+      search(query).then((res) => {
+        console.log("books", res)
+        this.setState((prevState) => ({
+          biblio:res,
+        }));
       });
-    });
-    this.setState({
-      found: booksArr
-    })
+    }
   };
 
-  componentDidMount() {
-    this.obtainDatabase();
-  }
   render() {
     return (
       <div className="search-books">
@@ -66,17 +37,16 @@ class SearchBooks extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.biblio ? (
-              this.state.found ? (
-                this.state.found.map((book) => (
+            {this.state.biblio.length
+              ? (
+                this.state.biblio.map((book) => (
                   <BookItem book={book} shelfHandler={this.props.shelfHandler} key={Math.random()} />
                 ))
-              ) : (
+              )
+              : (
                 "No Results..."
               )
-            ) : (
-              <Loader />
-            )}
+            }
           </ol>
         </div>
       </div>
